87bf60490f5b6f28acb93fda76bd19599580be2e
Lucile Lévêque <lululeveq@hotmail.fr>
2024-06-16 15:59:54 +0200

    Conversion methods implemented

--- python_implementation/genetic_algorithm/genetic_algorithm.py
+++ python_implementation/genetic_algorithm/genetic_algorithm.py
@@ -1,23 +1,35 @@
 # Résolution du problème avec algorithme génétique
 
+import json
 from model import *
-from pandas import DataFrame
+from pandas import DataFrame, Series
 import numpy as np
 
 class Planning():
-    def __init__(self, festival):
+    def __init__(self, festival, matrix=None, planning=None):
         self.festival = festival
-        self.matrix = None 
-        self.generate_matrix()
+        self.matrix = matrix
+        self.planning = planning
+        self.evaluation_score = 0
+        if not matrix and not planning:
+            self.generate_matrix()
+            self.conversion_matrix_to_planning()
+        elif not matrix and planning:
+            self.conversion_planning_to_matrix()
+        elif matrix and not planning:
+            self.conversion_matrix_to_planning()
+        else:
+            raise Exception("Only one of matrix or planning should be provided.")
         
     def generate_matrix(self):
         """
-        Generate a matrix where columns are players and time slots, and lines are proposed RPG.
+        Generate a pandas.DataFrame matrix where columns are players and time slots, and lines are proposed RPG.
         
         For example:
             - Players are: Alice, Bob and Chris.
             - Time slots are: saturday afternoon, saturday night and sunday afternoon.
             - Proposed RPG are: D&D, Alien and MYZ.
+        
         So the generated matrix will look like : 
                 | Alice | Bob   | Chris | sat. a. | sat. n. | sun. a. |
         D&D     | NaN   | NaN   | NaN   | NaN     | NaN     | NaN     |
@@ -47,11 +59,90 @@ class Planning():
     def planning_evaluation(self):
         # TODO
         pass
-    
-    def conversion_planning_to_matrix(self):
-        # TODO
-        pass
-    
+
     def conversion_matrix_to_planning(self):
-        # TODO
-        pass
+        """
+        Convert a pandas.DataFrame matrix format planning into a more readable JSON format.
+        The planning should be realistic (without any unsatisfied hard constraint) to be generated.
+        If the planning is not realistic, the generated JSON is empty.
+
+        For example, if the matrix planning is: 
+                | Alice | Bob   | Chris | sat. a. | sat. n. | sun. a. |
+        D&D     | 1     | 0     | 0     | 1       | 0       | 1       |
+        Alien   | 1     | 1     | 0     | 0       | 1       | 0       |
+        MYZ     | 1     | 0     | 1     | 1       | 0       | 0       |
+
+        This matrix planning is no realistic because D&D should not be played on saturday AND sunday afternoons.
+        The JSON format will be: {"games":[]}
+
+        But if the matrix planning is: 
+                | Alice | Bob   | Chris | sat. a. | sat. n. | sun. a. |
+        D&D     | 1     | 1     | 1     | 0       | 0       | 1       |
+        Alien   | 1     | 1     | 0     | 0       | 1       | 0       |
+        MYZ     | 1     | 0     | 1     | 1       | 0       | 0       |
+
+        The JSON format will be:
+        {"games":[
+            { "name": "D&D", "players":["Alice", "Bob", "Chris"], "timeslot":"sun. a."},
+            { "name": "Alien", "players":["Alice", "Bob"], "timeslot":"sat. n."},
+            { "name": "MYZ", "players":["Alice", "Chris"], "timeslot":"sat. a."}
+        ]}
+        """
+        if self.planning_evaluation() > 0:
+            games = []
+            for game_name, row in self.matrix.iterrows():
+                game_info = {"name": game_name, "players": [], "timeslot": None}
+                for col in self.matrix.columns:
+                    if col.startswith(('sat.', 'sun.')): # TODO: vérifier de manière moins empirique
+                        if row[col] == 1:
+                            game_info["timeslot"] = col
+                    else:
+                        if row[col] == 1:
+                            game_info["players"].append(col)
+                games.append(game_info)
+            self.planning = json.dumps({"games": games})
+        else:
+            self.planning = json.dumps({"games":[]})
+
+    def conversion_planning_to_matrix(self):
+        """
+        Convert a JSON format planning into a pandas.DataFrame matrix format.
+        The planning should not be empty to be generated.
+        If the planning is empty, the generated matrix is None.
+
+        For example, if the JSON format is:
+        {"games":[
+            { "name": "D&D", "players":["Alice", "Bob", "Chris"], "timeslot":"sun. a."},
+            { "name": "Alien", "players":["Alice", "Bob"], "timeslot":"sat. n."},
+            { "name": "MYZ", "players":["Alice", "Chris"], "timeslot":"sat. a."}
+        ]}
+
+        The matrix planning will be: 
+                | Alice | Bob   | Chris | sat. a. | sat. n. | sun. a. |
+        D&D     | 1     | 1     | 1     | 0       | 0       | 1       |
+        Alien   | 1     | 1     | 0     | 0       | 1       | 0       |
+        MYZ     | 1     | 0     | 1     | 1       | 0       | 0       |
+        """
+        games = self.planning["games"]
+        if len(games) > 0:
+            # Extraction des joueurs et créneaux horaires sans doublons
+            players = set()
+            timeslots = set()
+            for game in games:
+                players.update(game["players"])
+                timeslots.add(game["timeslot"])
+            # On met les players et time_slots en guise de colonnes
+            columns = list(players) + list(timeslots)
+            df = DataFrame(columns=columns)
+            # On ajoute les données au DataFrame
+            for game in games:
+                row = {player: 0 for player in players}
+                row.update({timeslot: 0 for timeslot in timeslots})
+                for player in game["players"]:
+                    row[player] = 1
+                if game["timeslot"]:
+                    row[game["timeslot"]] = 1
+                df = df.append(Series(row, name=game["name"]))
+            self.matrix = df
+        else:
+            self.matrix = None
